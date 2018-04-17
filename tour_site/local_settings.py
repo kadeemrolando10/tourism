834f3619532dad754b8d28510d57243ca12525f8
kadeem <kadeemrolando10@gmail.com>
2018-04-17 18:04:21 -0400

    fine function

--- tour_site/local_settings.py
+++ tour_site/local_settings.py
@@ -1,11 +1,10 @@
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tour_site',
+        'NAME': 'bd_django',
         'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
